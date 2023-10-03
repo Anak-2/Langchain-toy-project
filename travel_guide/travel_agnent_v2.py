5edce917c7e44e3d1dafcf62cc673ba5403ce105
donghyun <ilovekdh1208@ajou.ac.kr>
2023-10-03 23:48:34 +0900

    UPDATE travel agent

--- travel_guide/travel_agnent_v2.py
+++ travel_guide/travel_agnent_v2.py
@@ -1,21 +1,27 @@
+import json
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
 from langchain.prompts import StringPromptTemplate
 from langchain import OpenAI, SerpAPIWrapper, LLMChain
-from typing import List, Union
+from typing import Any, List, Union
 from langchain.schema import AgentAction, AgentFinish, OutputParserException
 import re
 from langchain.agents import initialize_agent, Tool
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import DuckDuckGoSearchRun
+from langchain.utilities import GoogleSerperAPIWrapper
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 import dotenv
 import os
 import chainlit as cl
+from datetime import datetime
 from langchain.vectorstores.base import VectorStore
 import faiss
 from langchain.vectorstores import FAISS
 from langchain.docstore import InMemoryDocstore
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.tools import GooglePlacesTool
+from langchain.output_parsers import CommaSeparatedListOutputParser
+from chainlit.input_widget import Select, Switch, Slider
 
 
 dotenv_file = dotenv.find_dotenv()
@@ -23,6 +29,8 @@ dotenv.load_dotenv(dotenv_file)
 
 # Define embedding model
 embeddings_model = OpenAIEmbeddings()
+
+search_tool = SerpAPIWrapper()
 
 # Initialize the vectorstore as empty
 embedding_size = 1536
@@ -32,80 +40,10 @@ index = faiss.IndexFlatL2(embedding_size)
 vectorstore = FAISS(embeddings_model.embed_query,
                     index, InMemoryDocstore({}), {})
 
-
-template = """Answer the following questions as best you can, but speaking as passionate travel expert. You must include at lest two tourist attraction per day in the travel itinerary. 
-Output the final travel itinerary in following format:
-Day 1:
-    Morning:
-        Breakfast:
-    Afternoon:
-        Lunch:
-    Evening:
-        Dinner:
-Day 2:
-    Morning:
-        Breakfast:
-    Afternoon:
-        Lunch:
-    Evening:
-        Dinner:
-Day 3:
-    Morning:
-        Breakfast:
-    Afternoon:
-        Lunch:
-    Evening:
-        Dinner:
-...
-
-Also, translate the final version of the travel itinerary into Korean language.
-You have access to the following tools:
-
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: a detailed day by day final answer to the original input question
-
-Output the final travel itinerary in following format:
-Day 1:
-    Morning:
-        Breakfast:
-    Afternoon:
-        Lunch:
-    Evening:
-        Dinner:
-Day 2:
-    Morning:
-        Breakfast:
-    Afternoon:
-        Lunch:
-    Evening:
-        Dinner:
-Day 3:
-    Morning:
-        Breakfast:
-    Afternoon:
-        Lunch:
-    Evening:
-        Dinner:
-...
-
-Begin! Remember to answer as a passionate and informative travel expert when giving your final answer.
-
-
-Question: {input}
-{agent_scratchpad}"""
-
-template_with_history = """Answer the following questions as best you can, but speaking as passionate travel expert. You must include at lest two tourist attraction per day in the travel itinerary.
-Also, you need to include at least two restraurants per day that reflects the traveler's preferences or dietary preferences. 
+template_with_history = """
+Answer the following questions as best you can, but speaking as passionate travel expert.
+You must include "airline ticket" and at lest two or more "tourist attraction" per day in the travel itinerary.
+Also, you need to include at least two or more "restraurants" per day. 
 
 You have access to the following tools:
 
@@ -120,15 +58,50 @@ Action Input: the input to the action
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+Final Answer: the final answer to the original input question. The final output should be a formatted like markdown as following schema
+Remember when getting information about a restaurant and touris attractions and festivals, bring an image and exact address of them that introduces places.
+```
+# Travel Itinerary 
 
-Begin! Remember to answer as a passionate and informative travel expert when giving your final answer in Korean.
+## Day n
+    // you should consider client's Departure date Return date
+    
+### Tourist Attractions
+    // Please write in text why you recommend this place
+    // When introducing a location to a client, you should include the actual location and images to explain it.
+    // Please write images in markdown format. like following schema
+    *** place's name ***
+    !["place's name"]("image url must use tool. Get it using the Search image tool")
+    
+### Restaurants
+    // Please write in text why you recommend this place
+    // When introducing a location to a client, you should include the actual location and images to explain it.
+    // Please write images in markdown format. like following schema
+    *** place's name ***
+    !["place's name"]("image url must use tool. Get it using the Search image tool")
+
+# Festivals
+    // Please write in text why you recommend this place
+    // When introducing a location to a client, you should include the actual location and images to explain it.
+    // Please write images in markdown format. like following schema
+    *** place's name ***
+    !["place's name"]("image url must use tool. Get it using the Search image tool")
+
+# Transportation
+    // Recommend transportation that is easy to use and well priced
+```
+
+Remember when getting information about a restaurant and touris attractions and festivals, bring an image and exact address of them that introduces places.
+
 
 Previous conversation history:
 {history}
 
 Question: {input}
-{agent_scratchpad}"""
+{agent_scratchpad}
+
+Begin! Remember to answer as a passionate and informative travel expert when giving your final answer in Korean.
+"""
 # Set up a prompt template
 
 
@@ -172,8 +145,7 @@ class CustomOutputParser(AgentOutputParser):
         regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
         match = re.search(regex, llm_output, re.DOTALL)
         if not match:
-            raise OutputParserException(
-                f"Could not parse LLM output: `{llm_output}`")
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
         action = match.group(1).strip()
         action_input = match.group(2)
         # Return the action and action input
@@ -181,29 +153,34 @@ class CustomOutputParser(AgentOutputParser):
 
 
 def search_online(input_text):
-    search = DuckDuckGoSearchRun().run(
+    search = search_tool.run(
         f"site:tripadvisor.com things to do{input_text}")
     return search
 
 
 def search_festivals(input_text):
-    search = DuckDuckGoSearchRun().run(
+    search = search_tool.run(
         f"site:tripadvisor.com festivals in{input_text}")
-    return search
-
-
-def search_hotel(input_text):
-    search = DuckDuckGoSearchRun().run(f"site:booking.com {input_text}")
-    return search
-
-
-def search_flight(input_text):
-    search = DuckDuckGoSearchRun().run(f"site:skyscanner.com {input_text}")
     return search
 
 
 def search_general(input_text):
     search = DuckDuckGoSearchRun().run(f"{input_text}")
+    return search
+
+
+def search_hotel(input_text):
+    search = search_tool.run(f"site:booking.com {input_text}")
+    return search
+
+
+def search_flight(input_text):
+    search = search_tool.run(f"site:skyscanner.com {input_text}")
+    return search
+
+
+def search_places(input_text):
+    search = GooglePlacesTool().run(f"{input_text}")
     return search
 
 
@@ -214,14 +191,90 @@ def _handle_error(error) -> str:
     return str(error)[:50]
 
 
+dest, start_date, end_date = None, None, None
+
+
+def _handle_error(error) -> str:
+    return str(error)[:50]
+
+
 @cl.on_chat_start
-def main():
+async def main():
+
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="OpenAI - Model",
+                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k",
+                        "gpt-4", "gpt-4-32k"],
+                initial_index=0,
+            ),
+            Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
+            Slider(
+                id="Temperature",
+                label="OpenAI - Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+            Slider(
+                id="SAI_Steps",
+                label="Stability AI - Steps",
+                initial=30,
+                min=10,
+                max=150,
+                step=1,
+                description="Amount of inference steps performed on image generation.",
+            ),
+            Slider(
+                id="SAI_Cfg_Scale",
+                label="Stability AI - Cfg_Scale",
+                initial=7,
+                min=1,
+                max=35,
+                step=0.1,
+                description="Influences how strongly your generation is guided to match your prompt.",
+            ),
+            Slider(
+                id="SAI_Width",
+                label="Stability AI - Image Width",
+                initial=512,
+                min=256,
+                max=2048,
+                step=64,
+                tooltip="Measured in pixels",
+            ),
+            Slider(
+                id="SAI_Height",
+                label="Stability AI - Image Height",
+                initial=512,
+                min=256,
+                max=2048,
+                step=64,
+                tooltip="Measured in pixels",
+            ),
+        ]
+    ).send()
+
     tools = [
+
+        Tool(
+            name="Search places",
+            func=search_places,
+            description="This is useful when search information of real places such as address and place image. You must research within 5 items should not over."
+        ),
 
         Tool(
             name="Search general",
             func=search_general,
             description="useful for when you need to answer general travel questions"
+        ),
+        Tool(
+            name="Search image",
+            func=GoogleSerperAPIWrapper(type="images").run,
+            description="useful for when you need to get image url"
         ),
         Tool(
             name="Search tripadvisor",
@@ -233,26 +286,19 @@ def main():
             func=search_hotel,
             description="useful for when you need to answer hotel questions"
         ),
+        # Tool(
+        #     name="Search flight",
+        #     func=search_flight,
+        #     description="useful for when you need to answer flight questions"
+        # ),
         Tool(
             name="Search festivals",
             func=search_festivals,
             description="useful for when you need to answer festivals in related areas"
-        ),
-        Tool(
-            name="Search flight",
-            func=search_flight,
-            description="useful for when you need to answer flight questions"
         )
 
     ]
 
-    prompt = CustomPromptTemplate(
-        template=template,
-        tools=tools,
-        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-        # This includes the `intermediate_steps` variable because that is needed
-        input_variables=["input", "intermediate_steps"]
-    )
     prompt_with_history = CustomPromptTemplate(
         template=template_with_history,
         tools=tools,
@@ -267,7 +313,6 @@ def main():
     llm_chain = LLMChain(llm=llm, prompt=prompt_with_history)
     tool_names = [tool.name for tool in tools]
     agent = LLMSingleActionAgent(
-        vectorstore=vectorstore,
         llm_chain=llm_chain,
         output_parser=output_parser,
         stop=["\nObservation:"],
@@ -277,8 +322,62 @@ def main():
     agent_executor = AgentExecutor.from_agent_and_tools(
         agent=agent, tools=tools, verbose=True, memory=memory)
 
+    departure = await cl.AskUserMessage(content="어디서 출발하나요?").send()
+    if departure:
+        await cl.Message(
+            content=f"출발지: {departure['content']}",
+        ).send()
+
+    dest = await cl.AskUserMessage(content="어디로 여행하고 싶으세요?").send()
+    if dest:
+        await cl.Message(
+            content=f"목적지: {dest['content']}",
+        ).send()
+
+    start_date = await cl.AskUserMessage(content="여행 시작일이 언제인가요? (월/일)", timeout=10).send()
+    if start_date:
+        await cl.Message(
+            content=f"Your travel starts at: {start_date['content']}",
+        ).send()
+
+    end_date = await cl.AskUserMessage(content="여행 종료일이 언제인가요? (월/일)", timeout=10).send()
+    if end_date:
+        await cl.Message(
+            content=f"You returns at: {end_date['content']}",
+        ).send()
+
+    # Define the date format
+    date_format = "%m/%d"
+    # Parse the date strings into date objects
+    date1 = datetime.strptime(start_date['content'], date_format)
+    date2 = datetime.strptime(end_date['content'], date_format)
+
+    date_difference = date2 - date1
+    days_difference = date_difference.days + 1
+
     # Store the chain in the user session
     cl.user_session.set("agent", agent_executor)
+
+    print(dest, start_date, end_date)
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("agent")  # type: LLMChain
+
+    message = f"I want to travel to {dest['content']} from {start_date['content']} to {end_date['content']}. Please plan my trip for me."
+    message_kor = f"나는 {start_date['content']}부터 {end_date['content']}까지 ({days_difference}일 동안) {departure['content']}에서 {dest['content']}로 여행하고 싶어. 나 대신에 여행 계획을 짜줘."
+
+    # Call the chain asynchronously
+    print(message_kor)
+    res = await llm_chain.acall(message_kor, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    print(res)
+    # Do any post processing here
+    print(res['output'])
+    # Send the response
+    # await cl.Message(content=res["output"]).send()
+
+
+@cl.on_settings_update
+async def setup_agent(settings):
+    print("on_settings_update", settings)
 
 
 @cl.on_message
